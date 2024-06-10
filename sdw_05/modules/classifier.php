db505d2fce9f7c4ca3cc83fa6928c06ed9ca72ca
vegan <enrico.leidner@mni.thm.de>
2024-06-10 18:50:00 +0200

    - updated classify_request function to account for more cases of malicious requests (content-injection, access-tool) at classifier.php - removed sql part of patterns array at classifier.php

--- sdw_05/modules/classifier.php
+++ sdw_05/modules/classifier.php
@@ -27,31 +27,22 @@ class Classifier
 
     public static function classify_request(): string
     {
+        // set request class
         $request_class = 'normal';
-        /*
-         * NEED BETTER IDEA THAN STATIC REG EX
-        */
 
-        // Key-Value Array of RegEx's for identifying harmful requests
-        $patterns = [
-            'config-grabber' => '/\/wp-config.php/i',
-            'sql-injection' => '/(UNION|SELECT|INSERT|UPDATE|DELETE|DROP|CREATE|ALTER)/i',
-            'xss-attack' => '/(<script>|%3Cscript%3E)/i',
-            //'brute-force' => '/(wp-login\.php\?action=login|xmlrpc\.php)/i',
-            //'file-access' => '/\/(searchreplacedb2\.php|wp-cron\.php|themes)/i',
-        ];
-
-        /**
-         * BRUTE FORCE DETECTION
-         **/
-
-        // database vars
+        // request vars
         $ip = $_SERVER['REMOTE_ADDR'];
         $uri = $_SERVER['REQUEST_URI'];
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+        // database vars
         global $wpdb;
         $brute_force_login_uri = '%wp-login%';
         $table_name = $wpdb->prefix . 'thm_security_access_log';
 
+        /**
+         * BRUTE FORCE DETECTION
+         **/
         // get login request count from database
         $login_count = $wpdb->get_var($wpdb->prepare(
             "SELECT COUNT(*) FROM $table_name WHERE client = %s AND url LIKE %s AND time > now() - interval 10 minute", $ip, $brute_force_login_uri
@@ -70,12 +61,37 @@ class Classifier
         }
 
         /**
+         * ACCESS TOOL DETECTION
+         **/
+        // List of viable user agents
+        $user_agents = "/windows|linux|fedora|ubuntu|macintosh|i-phone|i-pod|i-pad|android|wordpress/i";
+
+        // Check if user agent is viable
+        if (!preg_match($user_agents, $user_agent)) {
+            $request_class = 'access-tool';
+        }
+
+        /**
+         * SPAM DETECTION
+         **/
+        //...
+
+        /**
          * PATTERN DETECTION
          **/
+        // Key-Value Array of RegEx's for identifying harmful requests
+        $patterns = [
+            // pattern for the wp-config file
+            'config-grabber' => '/\/wp-config.php/i',
+            // pattern for scripts uses regEx expressions (NOT OPTIMAL!) [^e] --> find any char not between brackets (hello = hllo), n* --> matches if string contains zero or more occurrences of n
+            'content-injection' => '/(<script[^>]*>.*?<\/script>|<iframe[^>]*>.*?<\/iframe>)/i',
+            //'file-access' => '/\/(searchreplacedb2\.php|wp-cron\.php|themes)/i'
+        ];
 
         // Iterate over every key-value pair in the patterns array, class is set as the key and patterns as patterns
         foreach ($patterns as $class => $pattern) {
             // When the pattern matches the request return the correct class name
+            // Get info from uri or direct from content of request body
             if (preg_match($pattern, $uri) || preg_match($pattern, file_get_contents('php://input'))) {
                 $request_class = $class;
             }
